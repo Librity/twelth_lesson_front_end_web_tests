b842b1a22dfb19d9e9f7696b742749fb24e61100
Luis Geniole <luis.geniole@gmail.com>
2019-11-24 21:12:30 -0300

    test localStorage w/mock

--- src/components/TechList/index.js
+++ src/components/TechList/index.js
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // import { Container } from './styles';
 
 export default function TechList() {
   const [techs, setTechs] = useState([]);
   const [newTech, setNewTech] = useState('');
+
+  useEffect(() => {
+    const storedTechs = localStorage.getItem('techs');
+
+    if (storedTechs) {
+      setTechs(JSON.parse(storedTechs));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
 
   function handleAddTech() {
     setTechs([...techs, newTech]);
