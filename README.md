8745807fdc4daa75d14ac4a9063afd650d2d5302
Luis Geniole <luis.geniole@gmail.com>
2019-11-26 18:26:43 -0300

    add testCoverage

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # twelth_lesson_front_end_web_tests
+
+[JestDocs](https://jestjs.io/docs/en/getting-started)
--- __tests__/store/sagas/techs.test.js
+++ __tests__/store/sagas/techs.test.js
@@ -1,13 +1,33 @@
 import { runSaga } from 'redux-saga';
-import { getTechsSuccess } from '~/store/modules/techs/actions';
+import MockAdapter from 'axios-mock-adapter';
+import api from '~/services/api';
+
+import {
+  getTechsSuccess,
+  getTechsFailure,
+} from '~/store/modules/techs/actions';
 import { getTechs } from '~/store/modules/techs/saga';
+
+const apiMock = new MockAdapter(api);
 
 describe('Techs saga', () => {
   it('shpuld be abble to fetch techs', async () => {
     const dispatch = jest.fn();
 
+    apiMock.onGet('techs').reply(200, ['Node.js']);
+
     await runSaga({ dispatch }, getTechs).toPromise();
 
     expect(dispatch).toHaveBeenCalledWith(getTechsSuccess(['Node.js']));
+  });
+
+  it('shpuld fail when the api returns an error', async () => {
+    const dispatch = jest.fn();
+
+    apiMock.onGet('techs').reply(500);
+
+    await runSaga({ dispatch }, getTechs).toPromise();
+
+    expect(dispatch).toHaveBeenCalledWith(getTechsFailure());
   });
 });
